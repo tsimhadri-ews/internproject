76e7c2187641e27f66dc0c56aa0aceb6a508e24e
myra-everwatch <myra.cropper@everwatchsolutions.com>
2024-07-22 12:55:42 +0000

    test

--- Pipeline/Intrusion/Production/compile.py
+++ Pipeline/Intrusion/Production/compile.py
@@ -6,6 +6,8 @@ from read_file import read_file
 from train_op import train_op
 from model_eval_deploy import model_eval_deploy
 
+print("running file")
+
 check_condition_op = components.func_to_container_op(func=check_condition, base_image='python:3.7', packages_to_install=['pandas==1.1.5', 'sqlalchemy==1.4.45', 'boto3', 'psycopg2-binary'])
 
 read_csv_op = components.func_to_container_op(func=read_file, output_component_file='preprocess.yaml', base_image='python:3.7', packages_to_install=['pandas==1.1.5','scikit-learn==1.0.1', 'kfp', 'numpy', 'minio', 'psycopg2-binary', 'sqlalchemy==1.4.45','boto3'])
@@ -19,6 +21,7 @@ train_op = kfp.components.load_component_from_file('train.yaml')
 eval_deploy_op = kfp.components.load_component_from_file('eval_deploy.yaml')
 
 def ml_pipeline():
+    print("running pipeline")
     check_condition = check_condition_op()
     check_condition.execution_options.caching_strategy.max_cache_staleness = "P0D"
     with dsl.Condition(check_condition.output == 'True'):
