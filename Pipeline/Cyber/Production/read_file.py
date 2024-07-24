4448cb950fe6f55e311bd7a06c17c44298e0c1cb
myra-everwatch <myra.cropper@everwatchsolutions.com>
2024-07-24 19:00:25 +0000

    test

--- Pipeline/Cyber/Production/read_file.py
+++ Pipeline/Cyber/Production/read_file.py
@@ -116,8 +116,8 @@ def read_file() -> None:
 
     df = preprocess(df)
     
-    X = df.drop(columns=["attack_cat"])
-    y = df["attack_cat"]
+    X = df.drop(columns=["outcome"])
+    y = df["outcome"]
         
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     
